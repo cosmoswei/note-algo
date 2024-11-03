f229f0fec1f938fb81e7db30a8565fe2fa05f84d
宇宙伟 <2117008741@qq.com>
2024-11-03 21:20:07 +0800

    1

--- src/com/wei/leetcode/f1000/Solution_638.java
+++ src/com/wei/leetcode/f1000/Solution_638.java
@@ -6,13 +6,17 @@ import java.util.List;
 import java.util.Map;
 
 class Solution_638 {
+
     int ans = 0x3f3f3f3f;
     List<Integer> price, needs;
     List<List<Integer>> special;
     Map<Integer, Integer> map = new HashMap<>();
     int n, m;
+
     public int shoppingOffers(List<Integer> _price, List<List<Integer>> _special, List<Integer> _needs) {
-        price = _price; special = _special; needs = _needs;
+        price = _price;
+        special = _special;
+        needs = _needs;
         n = price.size();
         List<Integer> temp = new ArrayList<>();
         for (int i = 0; i < n; i++) temp.add(0);
@@ -40,17 +44,19 @@ class Solution_638 {
         dfs(0, needs, 0);
         return ans;
     }
+
     void dfs(int u, List<Integer> list, int cur) {
-        if (cur >= ans) return ;
+        if (cur >= ans) return;
         int cnt = 0;
         for (int i = 0; i < n; i++) cnt += list.get(i);
         if (cnt == 0) {
             ans = cur;
-            return ;
+            return;
         }
         if (u == m) return;
         List<Integer> x = special.get(u);
-        out:for (int k = 0; k <= map.get(u); k++) {
+        out:
+        for (int k = 0; k <= map.get(u); k++) {
             List<Integer> clist = new ArrayList<>(list);
             for (int i = 0; i < n; i++) {
                 int a = x.get(i), b = clist.get(i);
