f6c275e5682b95a842706d1be4bb6d868e1b0770
宇宙伟 <2117008741@qq.com>
2025-03-13 23:23:54 +0800

    1

--- src/com/wei/leetcode/f500/Solution_167.java
+++ src/com/wei/leetcode/f500/Solution_167.java
@@ -5,10 +5,27 @@ class Solution_167 {
     public static void main(String[] args) {
         Solution_167 solution128 = new Solution_167();
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        int i[] = solution128.twoSum(nums, 5);
+        int i[] = solution128.twoSum2(nums, 5);
         for (int j = 0; j < i.length; j++) {
             System.out.println("i = " + i[j]);
         }
+    }
+
+    public int[] twoSum2(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (target > sum) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1};
     }
 
     public int[] twoSum(int[] numbers, int target) {
