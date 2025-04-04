afbfe7d12b12f632e4ff3cb7ca8b432da847a836
宇宙伟 <2117008741@qq.com>
2025-04-04 22:29:21 +0800

    1

---
+++ src/com/wei/leetcode/f1500/Solution_1123.java
@@ -0,0 +1,34 @@
+package com.wei.leetcode.f1500;
+
+import com.wei.leetcode.TreeNode;
+
+public class Solution_1123 {
+
+    public static void main(String[] args) {
+        Solution_1123 solution = new Solution_1123();
+        System.out.println("solution = "
+                + solution.lcaDeepestLeaves(new TreeNode(1)).val);
+    }
+
+    private TreeNode res;
+    private int maxDepth;
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        dfs(root, 0);
+        return res;
+    }
+
+ final    public int dfs(TreeNode root, int dept) {
+        if (root == null) {
+            maxDepth = Math.max(maxDepth, dept);
+            return dept;
+        }
+        int left = dfs(root.left, dept + 1);
+        int right = dfs(root.right, dept + 1);
+        if (left == right && left == maxDepth) {
+            res = root;
+        }
+        return Math.max(left, right);
+    }
+
+}
