775251e88d4d2fc30a9817b4e80f53152b864433
宇宙伟 <2117008741@qq.com>
2025-04-07 20:10:32 +0800

    1

--- src/com/wei/leetcode/f500/Solution_416.java
+++ src/com/wei/leetcode/f500/Solution_416.java
@@ -1,16 +1,17 @@
 package com.wei.leetcode.f500;
 
 
+import java.util.Arrays;
+
 public class Solution_416 {
 
 
     public static void main(String[] args) {
         int[] arrays = {3, 9, 20, 2, 2, 15, 7};
         Solution_416 solution = new Solution_416();
-        Object res = solution.canPartition(arrays);
+        Object res = solution.canPartition2(arrays);
         System.out.println("res = " + res);
     }
-
 
     public boolean canPartition(int[] nums) {
         int sum = 0;
@@ -36,6 +37,40 @@ public class Solution_416 {
             }
         }
         return dp[n][target];
+    }
+
+    public boolean canPartition2(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int n = nums.length;
+        int target = sum / 2;
+        int[][] memo = new int[n][sum / 2 + 1];
+        for (int[] ints : memo) {
+            Arrays.fill(ints, -1);
+        }
+        return dfs(n - 1, sum / 2, nums, memo);
+    }
+
+    private boolean dfs(int i, int j, int[] nums, int[][] memo) {
+
+        if (i < 0) {
+            return j == 0;
+        }
+
+        if (memo[i][j] != -1) {
+            return memo[i][j] == 1;
+        }
+
+        boolean res = j >= nums[i] && dfs(i - 1, j - nums[i], nums, memo) || dfs(i - 1, j, nums, memo);
+
+        memo[i][j] = res ? 1 : 0;
+
+        return res;
     }
 }
 
