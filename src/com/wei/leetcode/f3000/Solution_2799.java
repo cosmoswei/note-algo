3a999bfa6ec5eed8f8d68dff278ea79a6d398766
宇宙伟 <2117008741@qq.com>
2025-04-24 23:25:09 +0800

    1

---
+++ src/com/wei/leetcode/f3000/Solution_2799.java
@@ -0,0 +1,38 @@
+package com.wei.leetcode.f3000;
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+class Solution_2799 {
+    public static void main(String[] args) {
+        Solution_2799 solution = new Solution_2799();
+        int res = solution.countCompleteSubarrays(new int[]{5, 5, 5, 5});
+        System.out.println("res = " + res);
+    }
+
+    public int countCompleteSubarrays(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int mum : nums) {
+            set.add(mum);
+        }
+        int k = set.size();
+        Map<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+        int left = 0;
+        for (int num : nums) {
+            map.merge(num, 1, Integer::sum);
+            while (map.size() == k) {
+                int out = nums[left];
+                if (map.merge(out, -1, Integer::sum) == 0) {
+                    map.remove(out);
+                }
+                left++;
+            }
+            res += left;
+        }
+        return res;
+    }
+}
