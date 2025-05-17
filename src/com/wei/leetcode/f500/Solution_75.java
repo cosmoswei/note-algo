271a7a001e153d03d1ec72188650e6f53c98304f
宇宙伟 <2117008741@qq.com>
2025-05-17 09:49:28 +0800

    1

---
+++ src/com/wei/leetcode/f500/Solution_75.java
@@ -0,0 +1,15 @@
+package com.wei.leetcode.f500;
+
+import java.util.Arrays;
+
+public class Solution_75 {
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 3};
+        new Solution_75().sortColors(nums1);
+    }
+
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+    }
+}
